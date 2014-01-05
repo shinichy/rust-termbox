7d15382e8d919373918e6a18798550c1f460bcca
Shinichi Katayama <shin1.katayama@gmail.com>
2014-01-04 21:52:28 -0800

    remove unused code from hello.rs

--- examples/hello.rs
+++ examples/hello.rs
@@ -8,10 +8,7 @@ fn main() {
     tb::init();
     tb::print(1, 1, tb::bold, tb::white, tb::black, ~"Hello, world!");
     tb::present();
-    // std::timer::sleep(std::uv_global_loop::get(), 1000);
-    //
     let mut timer = Timer::new().unwrap();
     timer.sleep(1000);
-
     tb::shutdown();
 }
